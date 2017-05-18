0e3a54d5bc23ba85e48bd47983757b78bbed8a0a
Nahom Abi <nahomagidew@gmail.com>
2017-05-17 21:20:30 -0700

    Initial Commit

---
+++ face_detect.py
@@ -0,0 +1,31 @@
+""" A script to detect faces in image
+using OpenCV Haarcascades.
+
+Author: Nahom Abi
+
+Date: 05/16/2017
+
+"""
+
+import cv2
+
+faceCascade = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
+image = cv2.imread('image.jpg')
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+faces = faceCascade.detectMultiScale(
+    gray,
+    scaleFactor=1.1,
+    minNeighbors=5,
+    minSize=(30, 30)
+)
+
+print("{} faces detected.".format(len(faces)))
+
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+cv2.imshow('Faces', image)
+cv2.imwrite('detected.jpg', image)
+
+cv2.waitKey(0)
